4a0c479c46f7ae48484835b06aba346d53851b72
Sam Morgen <samorgen14@gmail.com>
2021-07-11 10:45:40 -0700

    Added ability to delete notes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -11,4 +11,11 @@ router.post("/notes", (req, res) => {
   res.json(notes);
 });
 
+router.delete("/notes/:id", (req, res) => {
+  const { id } = req.params;
+  const projectIndex = notes.findIndex((p) => p.id == id);
+  notes.splice(projectIndex, 1);
+  res.json(notes);
+});
+
 module.exports = router;
