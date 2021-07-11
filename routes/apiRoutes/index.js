30b42ca8e6207ff5377ec41298247ae31d2d9d5f
Sam Morgen <samorgen14@gmail.com>
2021-07-11 10:37:06 -0700

    Added ability to post new notes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -5,4 +5,10 @@ router.get("/notes", (req, res) => {
   res.json(notes);
 });
 
+router.post("/notes", (req, res) => {
+  req.body.id = notes.length.toString();
+  notes.push(req.body);
+  res.json(notes);
+});
+
 module.exports = router;
